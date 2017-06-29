e9d3211f9de284ffbddc8dd4c5048deaddcc8ed7
Rich Moyse <rich@moyse.us>
2017-06-29 11:29:58 -0400

    test to barely test http server functionality

--- simple/server_test.go
+++ simple/server_test.go
@@ -1,6 +1,9 @@
 package server
 
 import (
+	"log"
+	"net/http"
+	"os"
 	"testing"
 
 	"github.com/WhisperingChaos/termChan"
@@ -8,6 +11,20 @@ import (
 
 func TestUP(t *testing.T) {
 	term := termChan.NewAck(termChan.Start())
-	Start(TarServer, term)
+	debug := log.New(os.Stderr, "simple server:", log.LstdFlags|log.Lshortfile)
+	var opts Opts
+	opts.Routes = append(opts.Routes, happy(t))
+	Start(opts, term, debug)
 	term.Wait()
+}
+
+func happy(t *testing.T) (rt RouteHandler) {
+	fh := func(w http.ResponseWriter, r *http.Request) {
+		t.Log(os.Stderr, "Processing happy request.")
+		defer r.Body.Close()
+		return
+	}
+	rt.Route = `/happy`
+	rt.Handler = fh
+	return
 }
